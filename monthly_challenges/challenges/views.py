7ef9844c6ea675b8da50895d6e2b3e2ecdd12fc6
Ukabix <54454097+Ukabix@users.noreply.github.com>
2021-11-09 18:22:21 +0100

    adding redirects to months given by number

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,33 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    "january": "Work out for 1 hour everyday!",
+    "february": "Read a book for 30 min everyday!",
+    "march": "Learn Django for 30 mins everyday!",
+    "april": "Pick up flowes twice a week!",
+    "may": "Play bass for an hour every evening!",
+    "june": "Clean your appartment every day!",
+    "july": "Listen to new music every week!",
+    "august": "Drink milk every morning!",
+    "september": "Play chess once a week!",
+    "october": "Sleep longer every weekend!",
+    "november": "Cut on half coffee every morning!",
+    "december": "Learn JS 1 hour everyday!"
+}
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("Work out for 1 hour everyday!")
-
-
-def february(request):
-    return HttpResponse("Read a book for 30 min everyday!")
-
-
-def march(request):
-    return HttpResponse("Learn Django for 30 mins everyday!")
-
-
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+    redirect_month = months[month -1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 
 def monthly_challenge(request, month):
-    challenge_text = ""
-    if month == "january":
-        challenge_text = "Work out for 1 hour everyday!"
-    elif month == "february":
-        challenge_text = "Read a book for 30 min everyday!"
-    elif month == "march":
-        challenge_text = "Learn Django for 30 mins everyday!"
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This month is not supported")
-    return HttpResponse(challenge_text)
